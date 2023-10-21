48a2b665f6c7f831f2987beb4d199548c3d2536d
BernatRosello <63212761+BernatRosello@users.noreply.github.com>
2023-10-21 14:27:34 +0200

    Ported wsk/jerkin

---
+++ keyboards/wsk/jerkin/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8E, 0x75, 0x92, 0x98, 0xFF, 0xD1, 0x5A, 0x4B}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
---
+++ keyboards/wsk/jerkin/keymaps/vial/rules.mk
@@ -0,0 +1,6 @@
+# Build Options
+#   change yes to no to disable
+#
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
